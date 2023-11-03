de44327beab4232a00098ecb69b242c416fb3af2
Bruno Silva Rodrigues <78625466+BrunoSilva03@users.noreply.github.com>
2023-11-03 10:02:29 -0300

    Add files via upload

---
+++ trabalhando-com-formularios-react-pt2.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    {/* definindo o valor de email de uma vez inicialmente ele(o usuário) não pode mudar 
+          o valor de email */}
+    this.state = {
+      email: 'pandalindinho@gmail.com',
+      senha: '',
+      sexo: 'masculino'
+    }
+
+     this.insereEmail = this.insereEmail.bind(this);
+     this.trocaSexo = this.trocaSexo.bind(this); 
+  }
+
+  
+
+  insereEmail(e) {
+    {/* agora dá para inserir um novo email mesmo com o valor padrão vindo como
+      pandalindinho@gmail.com*/};
+    let valorDigitado = e.target.value;
+    this.setState({email: valorDigitado});
+  }
+
+  trocaSexo(e) {
+    this.setState({sexo: e.target.value});
+  }
+
+  render() {
+    return(
+      <>
+        <h1>Login:</h1>
+        email:
+        {/* definindo o valor de email de uma vez inicialmente ele(o usuário) não pode mudar o valor de email */}
+      
+        <input type="email" id="idemail" name="email" value={this.state.email} onChange={this.insereEmail}></input>
+        <br></br>
+        <br></br>
+
+        senha:
+        <input type="password" id="idsenha" name="senha" value={this.state.senha}
+        onChange={(e) => this.setState({senha: e.target.value})}></input>
+        {/* Aqui fez a mesmo coisa que fez no método insereEmail, só que em uma linha só.*/}
+        <br></br>
+        <br></br>
+
+
+        sexo:
+        <select id="idselect" value={this.state.sexo} onChange={this.trocaSexo}>
+          <option>Masculino</option>
+          <option>Feminino</option>
+        </select>
+      </>
+    )
+  }
+}
+
+export default App
